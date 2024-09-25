b373d3fddcc48c104395b6a94a10671efaf5fb45
Andrii Kyrch <andrii.kyrch@gmail.com>
2024-09-25 19:43:18 +0200

    Make task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const formElem = document.querySelector('.login-form');
 
+formElem.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const data = {
+    email: formElem.elements.email.value.trim(),
+    password: formElem.elements.password.value.trim(),
+  };
+
+  if (data.email === '' || data.password === '') {
+    alert(`All form fields must be filled in`);
+  } else {
+    console.log(data);
+  }
+  formElem.reset();
+});
